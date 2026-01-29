f1464220976b8daa421810da9076ebf0bbda8206
R Amarthya Sreechand <amarthyasreechand@gmail.com>
2026-01-29 11:11:49 +0530

    Build client, serve from backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const app = express();
 import morgan from 'morgan'
 import helmet from 'helmet';
-import cors from 'cors';
+// import cors from 'cors';
 
 import type { NextFunction, ErrorRequestHandler } from 'express';
 import { apiRouter } from './routers/api.router';
@@ -20,28 +20,34 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next: NextFunction) =>
 
 
 
-const corsOptions ={
-    origin: "https://textshare-client-virid.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}
-
-app.use(cors(corsOptions));
-app.options("/*splat", cors(corsOptions));
+// const corsOptions ={
+//     origin: "https://textshare-client-virid.vercel.app",
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// }
+//
+// app.use(cors(corsOptions));
+// app.options("/*splat", cors(corsOptions));
 
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("common"));
 }
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Server is working" });
-})
+// app.get('/', (req, res) => {
+//     return res.json({ message: "Server is working" });
+// })
 app.use('/api', apiRouter);
 app.get('/p/:id', apipasteHtmlGet);
+
+
+app.use((req, res) => {
+    console.log("404 path not found");
+    return res.status(404).send("Request to unknown path");
+})
 
 app.use(errorHandler);
 
